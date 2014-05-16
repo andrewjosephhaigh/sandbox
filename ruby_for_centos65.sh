6088e0d062665d2cc743d27e1126aec402492825
Andrew Haigh <andrew.haigh@cellsoftware.co.uk>
2014-05-16 15:35:06 +0100

    committed

---
+++ ruby_for_centos65.sh
@@ -0,0 +1,24 @@
+#/bin/bash
+
+#sudo yum -y groupinstall 'Development Tools'
+sudo yum install -y httpd-devel openssl-devel zlib-devel gcc gcc-c++ curl-devel expat-devel gettext-devel patch readline readline-devel zlib zlib-devel libyaml-devel libffi-devel make bzip2 zlib1g mysql-server
+wget http://pyyaml.org/download/libyaml/yaml-0.1.6.tar.gz
+tar zxvf yaml-0.1.6.tar.gz
+cd yaml-0.1.6
+./configure --prefix=/usr/local
+make
+#make install
+
+cd
+wget http://ftp.ruby-lang.org/pub/ruby/2.1/ruby-2.1.2.tar.gz
+tar zxvf ruby-2.1.2.tar.gz
+cd ruby-2.1.2
+./configure
+make
+#make install
+
+cd
+wget http://production.cf.rubygems.org/rubygems/rubygems-2.2.2.tgz
+cd rubygems-2.2.2
+
+#gem install bundler
